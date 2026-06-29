bbb0a1d07053a08bbebeec922f328ba2fcdfea2a
Nhvuu918 <luuhbaonguyen@dtu.edu.vn>
2026-06-28 16:53:41 -0900

    visual

--- visual.py
+++ visual.py
@@ -8,37 +8,42 @@ import board
 import time
 
 
-led = LED(17)
-dht = adafruit_dht.DHT11
-pin = 18
+dht=adafruit_dht.DHT11(board.D18)
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-temp=dht.temperature
-hum=dht.humidity  
+try:
+	temp=dht.temperature
+	hum=dht.humidity  
+except RuntimeError:
+	temp=0
+	hum=0
 print(f"{timestamp} | Nhiet do: {temp} do C | Do am: {hum} %")
 
 with open("log.txt","a") as f:
 	f.write(f"{timestamp} {temp} do C {hum} % \n")
 		
-fig=plt.firgure()
+fig=plt.figure()
 ax = plt.axes(xlim=(0,30), ylim=(15,45))
 max_points=30
 line, = ax.plot(np.arange(max_points),
-				np.ones(max_points,dtype=np.float)*np.nan, lw=1, c='blue', marker='d', ms=2)
+				np.ones(max_points,dtype=float)*np.nan, lw=1, c='blue', marker='d', ms=2)
 
 def init():
 	return line
 	
-h,t=adafruit_dht.read_retry(dht,pin)
-
 def animate(i):
-	h,t=adafruit_dht.read_retry(dht,pin)
-	y=t
-	old_y=line.get_ydata()
-	new_y=np.r_[old_y[1:],y]
-	line.set_ydata(new_y)
+	try:
+		t=dht.temperature
+	except RuntimeError:
+		return line
+		
+	if t is not None:
+		y=t
+		old_y=line.get_ydata()
+		new_y=np.r_[old_y[1:],y]
+		line.set_ydata(new_y)
 	return line,
 	
-anim = animation.FuncAnimation(fig,animate,init_func=init,frames=200,interval=20,blit=False)
+anim = animation.FuncAnimation(fig,animate,init_func=init,frames=2000,interval=20,blit=False)
 plt.show()	
 time.sleep(2)
 
